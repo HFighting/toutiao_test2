e6284ca4d38c3f50c66a36c03e225aa04c55baef
HFighting <41809761+HFighting@users.noreply.github.com>
2018-09-06 13:15:36 +0800

    Signed-off-by: HFighting <41809761+HFighting@users.noreply.github.com> 牛客网上剑指offer中的题

--- toutiao_test/src/toutiao_test/Solution.java
+++ toutiao_test/src/toutiao_test/Solution.java
@@ -1,10 +1,12 @@
 package toutiao_test;
 
+
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
-import javax.print.attribute.ResolutionSyntax;
 
 import org.junit.Test;
 
@@ -277,4 +279,322 @@ public class Solution {
 			fiNode(node.right, k);
 		}
 	}
+	/*
+	 * 请实现两个函数，分别用来序列化和反序列化二叉树
+	 */
+
+	String Serialize(TreeNode root) {//序列化二叉树，按照先序遍历
+		StringBuilder sb = new StringBuilder();
+		serialize(root, sb);
+		return sb.toString();
+	}
+	public void serialize(TreeNode node,StringBuilder sb) {
+		if (node!=null) {
+			sb.append(node.val+"!");
+			Serialize(node.left);
+	        Serialize(node.right);
+		}else {
+			sb.append("#!");
+			return;
+		}       
+	}
+	int index = 0;
+	TreeNode Deserialize(String str) {//反序列化二叉树
+		String[] strings = str.split("!");
+		TreeNode node = null;
+	    if (index < strings.length) {
+	    	if ("#".equals(strings[index])) {
+	    		node = new TreeNode(Integer.parseInt(strings[index]));//建立结点，先序遍历
+		    	index++;
+				node.left = Deserialize(str);//递归左子树
+				node.right = Deserialize(str);//递归右子树
+				return node;
+	    	}else {
+				index++;
+				return null;
+			}
+		}else {
+			 return node;
+		}	   
+	}
+	/*
+	 * 利用队列实现层序遍历二叉树，每层打印一行
+	 */
+	ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
+		ArrayList<ArrayList<Integer> > list = new ArrayList<>();
+		getNode(pRoot, list);
+		return list;
+    }
+	void getNode(TreeNode root,ArrayList<ArrayList<Integer>> list){
+		
+		Queue<TreeNode> queue = new LinkedList<>();
+		if (root==null) {
+			return;
+		}
+		queue.add(root);
+		while (!queue.isEmpty()) {//利用链表队列，在更改链表的时候遍历链表
+			ArrayList<Integer> nodes = new ArrayList<>();
+			int i = 0;int size = queue.size();
+			while (i++<size) {
+				TreeNode treeNode = queue.poll();
+				nodes.add(treeNode.val);
+				if (treeNode.left!=null) {
+					queue.add(treeNode.left);
+				}
+				if (treeNode.right!=null) {
+					queue.add(treeNode.right);
+				}
+			}
+			list.add(nodes);
+		}
+	}
+	/*
+	 * 按照之字形打印二叉树，即第一行从左到右打印二叉树，第二行从右到左打印二叉树，依次类推
+	 */
+	public ArrayList<ArrayList<Integer> > Print1(TreeNode pRoot) {
+		ArrayList<ArrayList<Integer> > list = new ArrayList<>();
+		printzhi(pRoot, list);
+		return list;
+    }
+	public void printzhi(TreeNode root,ArrayList<ArrayList<Integer> > list) {
+		if (root==null) {
+			return;
+		}
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
+		boolean flag = true;
+		while (!queue.isEmpty()) {
+			ArrayList<Integer> nodes = new ArrayList<>();
+			if (flag) {
+				int i = 0 ;
+				int size = queue.size();
+				while(i++<size) {
+					TreeNode treeNode = queue.poll();//出队列
+					nodes.add(treeNode.val);
+					if (treeNode.left!=null) {
+						queue.add(treeNode.left);
+					}
+					if (treeNode.right!=null) {
+						queue.add(treeNode.right);
+					}
+				}
+				flag = !flag;
+			}else {
+				int i = 0 ;
+				int size = queue.size();
+				Stack<Integer> stack = new Stack<>();
+				while(size-->i) {
+					TreeNode treeNode = queue.poll();//出队列
+					stack.push(treeNode.val);
+					if (treeNode.left!=null) {
+						queue.add(treeNode.left);
+					}
+					if (treeNode.right!=null) {
+						queue.add(treeNode.right);
+					}
+				}
+				while(!stack.isEmpty()) {
+					nodes.add(stack.pop());
+				}
+				flag = !flag;
+			}
+			list.add(nodes);
+		}
+	}
+	/*
+	 * 判断一棵二叉树是否是对称树
+	 */
+	boolean isSymmetrical(TreeNode pRoot)
+    {
+		return false;
+    }
+	/*
+	 * 删除一个排序链表中重复的节点1->2->2->3->4->4->5
+	 * 处理后：1->2->3->4->5
+	 */
+	public ListNode deleteDuplication(ListNode pHead)
+    {
+		if (pHead==null) {//链表为空的情况
+			return null;
+		}
+		if (pHead.next==null) {//链表只有一个元素的情况
+			return pHead;
+		}
+		ListNode firstlist = pHead;
+		while (firstlist!=null) {
+            if(firstlist.next!=null){
+                if (firstlist.val==firstlist.next.val) {
+				    firstlist.next = firstlist.next.next;
+			    }else {
+				    firstlist = firstlist.next;
+			    }
+            }else{
+                break;
+            }
+		}
+		return pHead;
+    }
+	/*
+	 * 删除一个排序链表中重复的节点1->2->2->3->4->4->5
+	 * 处理后：1->3->5
+	 */
+	public ListNode deleteDuplication1(ListNode pHead) {
+		if (pHead==null) {//链表为空的情况
+			return null;
+		}
+		if (pHead.next==null) {//链表只有一个元素的情况
+			return pHead;
+		}
+		ListNode current;
+		//如果前面几个相等
+		if (pHead.val == pHead.next.val) {
+			current = pHead.next.next;
+			if (current==null) {
+				return null;
+			}
+			while (current.val==pHead.val) {
+				current = current.next;
+				if (current==null) {
+					return null;
+				}
+			}
+			//到这为止，把最开始重复的全部已经去掉，接下来继续删除后面的
+			return deleteDuplication1(current);
+		}else {//若前面两个不相等
+			current = pHead.next;
+			//则从第二个开始删除递归
+			pHead.next = deleteDuplication1(current);
+			return pHead;
+		}
+	}
+	/*
+	 * 给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
+	 * 经典思路：
+	 * p1、p2同时指向头结点，p1每次走一步，p2每次走两步，当p1与p2相等时，p1走了x步，p2走了2x步，
+	 * 相当于p2比p1多走了一个环的步数，即x就是环中节点的个数；此时让p2指向头结点，并且每次只走一步，
+	 * 当p1=p2时，p1就是环的入口节点
+	 */
+	public ListNode EntryNodeOfLoop(ListNode pHead)
+    {
+		if (pHead==null) {
+			return null;
+		}
+		if (pHead.next==null) {
+			return null;
+		}		
+       return findhuan(pHead);
+    }	
+	
+	public ListNode findhuan(ListNode phead) {
+		ListNode p1 = phead;
+		ListNode p2 = phead;//p1\p2同时指向头结点
+		do {
+			p1 = p1.next;//p1走一步
+			p2 = p2.next.next;//p2走两步
+		} while (p1!=p2);
+		p2 = phead;
+		while (p1!=p2) {
+			p1 = p1.next;
+			p2 = p2.next;
+		}
+		return p1;
+	}
+	
+	/*
+	 * 请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。
+	 * 例如，字符串"+100","5e2","-123","3.1416"和"-1E-16"都表示数值。
+	 * 但是"12e","1a3.14","1.2.3","+-5"和"12e+4.3"都不是。
+	 */
+	@Test
+	public void isNumeric() {
+		String string = "-1E-16";
+        String match1 = "[+|-]?[\\.]?((\\d+)|(\\d+(e|E)[-|+]?\\d+))";
+        String match2 = "\\d+(\\.)\\d+";
+        String match3 = "\\d+(\\.)(\\d+(e|E)[-|+]?\\d+)";
+        if (string.matches(match1)||string.matches(match2)||string.matches(match3)) {
+			System.out.println(true);
+		}else {
+			System.out.println(false);
+		}		
+    }
+	
+	/*
+	 * 请实现一个函数用来匹配包括'.'和'*'的正则表达式。
+	 * 模式中的字符'.'表示任意一个字符，而'*'表示它前面的字符可以出现任意次（包含0次）。 
+	 * 在本题中，匹配是指字符串的所有字符匹配整个模式。
+	 * 例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但是与"aa.a"和"ab*a"均不匹配
+	 */
+	//前面一个是字符串，后面一个是模式
+	/*思路：
+	 * 若模式的第二个是*
+	 * 	字符第一个与模式的第一个字符相匹配，若匹配不成功，则模式后移两个字符，继续匹配
+	 * 	若匹配成功：
+	 * 		1.模式后移两位，继续匹配（相当于出现0次的情况）
+	 * 		2.模式后移一位，字符后移一位，继续匹配（相当于出现1次的情况）
+	 * 		3.字符后移一位，模式继续匹配（相当于出现多次的情况）
+	 * 若模式第二个不是*
+	 * 字符第一个与模式第一个相匹配，若失败，则返回false，若成功，则继续匹配，若匹配到.，则字符、模式都后移一位继续匹配
+	 */	
+	public boolean match(char[] str, char[] pattern)
+    {
+		if (str==null&&pattern==null) {
+			return false;
+		}
+        return findmatch(str, 0, pattern, 0);
+    }
+	public boolean findmatch(char[] str,int length1,char[] pattern,int length2) {
+		//如果都到尾，则匹配成功
+		if (length1==str.length&&length2==pattern.length) {
+			return true;
+		}
+		//若字符没到尾，模式到尾了，则匹配失败，或者是模式到尾了，字符没到尾，都匹配失败
+		if ((length2!=pattern.length&&length1==str.length)||(length1!=str.length&&length2==pattern.length)) {
+			return false;
+		}
+		if (length2+1<pattern.length&&pattern[length2+1]=='*') {
+			if (str[length1]==pattern[length2]) {
+				return findmatch(str, length1, pattern, length2+2)||
+						findmatch(str, length1+1, pattern, length2+1)||
+						findmatch(str, length1+1, pattern, length2);
+			}else {
+				return findmatch(str, length1, pattern, length2+2);
+			}
+		}
+		if (length2+1<pattern.length&&pattern[length2+1]!='*') {
+			if (str[length1]==pattern[length2]) {
+				return findmatch(str, length1+1, pattern, length2+1);
+			}else if(pattern[length2]=='.'){
+				return findmatch(str, length1+1, pattern, length2+1);
+			}else {
+				return false;
+			}
+		}
+		if (str==null&&pattern[length2]=='.'&&pattern[length2]=='*') {
+			return true;
+		}
+		return false;
+	}
+	
+	/*
+	 * 给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],
+	 * 其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。
+	 * 不能使用除法
+	 * 通俗理解：就是每一次把A中除下标为i的数字外的所有数字相乘得到B[i]的值
+	 */
+	public int[] multiply(int[] A) {
+		int[] B = new int[A.length];
+		for (int i = 0; i < A.length; i++) {
+			int temp = 1;
+			for (int j = 0; j < B.length; j++) {
+				if (j==i) {
+					continue;
+				}else {
+					temp *= A[j];
+				}
+			}
+			B[i] = temp;
+		}
+		return B;
+    }
+	
 }
